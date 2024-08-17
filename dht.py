724325f8efef1bca19ff76e6e0d12623f48de150
ofajdo <kiuchiryou@icloud.com>
2024-08-17 18:23:58 +0900

    diff追加

--- dht.py
+++ dht.py
@@ -3,6 +3,7 @@ import board
 import adafruit_dht
 import datetime
 import csv
+import numpy as np
 
 def Device(port):
     return  adafruit_dht.DHT22(port, use_pulseio=False)
@@ -23,6 +24,7 @@ def measure(device, path):
             writer = csv.writer(file)
             writer.writerow(result)
 
+        return([temperature, humidity, discomfort])
     except RuntimeError as error:
         time.sleep(0.1)
         measure(device, path)
@@ -30,5 +32,19 @@ def measure(device, path):
     except Exception as error:
         Device(device).exit()
 
-measure(board.D4, "./log1.csv")
-measure(board.D18, "./log2.csv")
+def diff(value1, value2, path):
+    dt_now = datetime.datetime.now().strftime("%Y/%m/%d %H:%M")
+    array1 = np.array(value1)
+    array2 = np.array(value2)
+
+    array = array2 - array1
+    array = np.round(array, decimals=1)
+    array = np.append(dt_now, array)
+
+    print(array)
+
+    with open(path, 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow(array)
+
+diff(measure(board.D4, "./log1.csv"),measure(board.D18, "./log2.csv"),"./diff.csv")
